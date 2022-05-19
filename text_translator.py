53e3abae445c2e52789478699fc56c2a09692d14
MohamadsalehMoradpoor <104715422+MohamadsalehMoradpoor@users.noreply.github.com>
2022-05-19 15:30:08 +0430

    Update text_translator.py

--- text_translator.py
+++ text_translator.py
@@ -1,4 +1,4 @@
 from deep_translator import GoogleTranslator
-to_translate = 'باعث افتخار من است که در خدمت شما هستم'
+to_translate = 'سخته, ولی اخرش قشنگه'
 translated = GoogleTranslator(source='fa', target='en').translate(to_translate)
 print(translated)
